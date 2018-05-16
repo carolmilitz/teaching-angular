e43bd920e9524a15673ce391f63349ac0e18b34a
Caroline Militz <caroline.militz@gmail.com>
2018-05-16 13:19:27 +0100

    fix styles

--- angular-directives/src/app/app.component.ts
+++ angular-directives/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: ['.white{color:#fff;}']
 })
 export class AppComponent {
   passwordVisible = false;
